b211c5ac2744d572b006886ed90839d8c6ddb3e9
JoaoGMagri <jgmagri@hotmail.com>
2022-11-23 20:31:36 -0300

    fix: console de erro

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -39,10 +39,10 @@ export async function getUserInfo(req, res) {
 
         delete InfoUser._id;
         delete InfoUser.password;
-        
+
         res.status(200).send(InfoUser);
     } catch (error) {
-        console.log(err);
+        console.log(error);
         res.status(500).send({ message: err.message });
     }
 }
