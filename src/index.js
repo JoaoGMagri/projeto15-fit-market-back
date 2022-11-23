38b368b9af8149fb64523a6af0ede869d016e345
JoaoGMagri <jgmagri@hotmail.com>
2022-11-22 22:34:18 -0300

    Arrumando para deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import express from 'express';
 import cors from 'cors';
 import joi from 'joi'
-import { collectionProducts } from "../database/database.js";
 import usersRouters from "./routers/users.routers.js"
 
 const app = express();
@@ -21,14 +20,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("Server running in port: 5000")
 });
-
-app.get("/products", async (req, res) => {
-
-    try {
-        const products = await collectionProducts.find().toArray()
-        res.send(products);
-    } catch (error) {
-        res.send(error);
-    }
-
-})
--- src/routers/users.routers.js
+++ src/routers/users.routers.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { postSingUp, postSingIn, deleteGoOut } from "../controllers/users.controller.js"
+import { postSingUp, postSingIn, deleteGoOut, getProducts } from "../controllers/users.controller.js"
 import { singUpMD } from "../middleware/singUp.middleware.js";
 import { singInMD } from "../middleware/singIn.middleware.js";
 
@@ -9,5 +9,6 @@ const router = Router();
 router.post("/sing-up", singUpMD, postSingUp);
 router.post("/sing-in", singInMD, postSingIn);
 router.delete( "/go-out", deleteGoOut );
+router.get("/products", getProducts )
 
 export default router;
